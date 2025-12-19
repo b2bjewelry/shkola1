6de23950966afa4668f0b8bb83df97cf6a37c51a
Pavel <nedogarokps07@gmail.com>
2025-12-19 15:30:13 +0200

    kkk

--- dz8.py
+++ dz8.py
@@ -1,10 +1,13 @@
 from pywebio.input import input as ps_input
-from pywebio.output import put_text, put_button, clear, put_error, put_success, put_html
+from pywebio.output import put_text, put_button, clear, put_error, put_success, put_html, put_warning
 from pywebio.input import actions
 from pywebio.output import clear
 
 import question
 
+
+# Инициализируем счетчик очков
+score = 0
 #Заголовок
 put_html('<h1>Привет  Давай пройдем опрос. </h1>')
 
@@ -15,6 +18,7 @@ compare_1 = question.ANSWER_1 == question_1
 # Проверяем условие если верно или нет
 if compare_1:
     put_success('✅ Верно!')
+    score += 1
 else:
     put_error('❌ Неверно')
 
@@ -26,6 +30,7 @@ compare_2 = question.ANSWER_2 == question_2
 
 if compare_2:
     put_success('✅ Верно!')
+    score += 1
 else:
     put_error('❌ Неверно')
 
@@ -37,6 +42,7 @@ compare_3 = question.ANSWER_3 == question_3
 
 if compare_3:
     put_success('✅ Верно!')
+    score += 1
 else:
     put_error('❌ Неверно')
 
@@ -48,6 +54,7 @@ compare_4 = question.ANSWER_4 == question_4
 
 if compare_4:
     put_success('✅ Верно!')
+    score += 11
 else:
     put_error('❌ Неверно')
 
@@ -59,7 +66,8 @@ compare_5 = question.ANSWER_5 == question_5
 
 if compare_5:
     put_success('✅ Верно!')
+    score += 1
 else:
     put_error('❌ Неверно')
 
-actions(label='', buttons=['Далее'])
+put_warning(f'Вы ответили на {score} вопроса')
